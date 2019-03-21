a1802ef4580d74e2a2044e5bcd567835cad4331e
Jason Lee <jasonlee@dhcp-v075-095.mobile.uci.edu>
2019-03-21 12:44:00 -0700

    started question template

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,13 @@
 $("#start").on("click", function(){
     $("#start").remove(); 
-})
+});
+
+var questions = [{
+    question: "The average person uses the toilet 2,500 times per year. So over the course of an average lifetime, people spend about ______ years on the toilet!",
+    answers: ["1", "3", "10", "10,000"],
+    correctAnswer: "3",
+    image: "assets/images/toiletclock.jpg"
+}, {
+    
+}
+]
