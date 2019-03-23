1de89baa0f2e11462f45a7f786b968fbb2dd26e1
Jason Lee <jasonlee@Jasons-MacBook-Pro.local>
2019-03-23 10:57:37 -0700

    finished javascript logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,10 @@ $("#start").on("click", function(){
 // invokes function(e) when answer-button is clicked
 $(document).on('click', '.answer-button', function(e){
     game.clicked(e);
+});
+
+$(document).on('click', '#reset', function() {
+    game.reset();
 })
 
 // Question Array
@@ -53,6 +57,7 @@ var game = {
     counter:30,
     correct:0,
     incorrect:0,
+    unanswered:0,
     countdown: function() {
         // Countdown from 30 seconds
         game.counter--;
@@ -67,18 +72,50 @@ var game = {
     loadQuestion: function() {
         // Countdown one number per second
         timer = setInterval(game.countdown, 1000);
+        $('#subwrapper').html("<h2>Time Remaining: <span id='counter'>30</span></h2>")
         // Add header for question
-        $("#subwrapper").html("<h2>" + questions[game.currentQuestion].question + "</h2>");
-        // load the question while adding a data-name to store the question
+        $("#subwrapper").append("<h2>" + questions[game.currentQuestion].question + "</h2>");
+        // Load the question while adding a data-name to store the question
         for (var i = 0; i < questions[game.currentQuestion].answers.length; i++) {
             $("#subwrapper").append('<button class="answer-button" id="button-' + i + '" data-name="' + questions[game.currentQuestion].answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>');
         }
     },
+    nextQuestion: function() {
+        // Reset counter to 30 seconds
+        game.counter = 30;
+        // Update HTML
+        $('#counter').html(game.counter);
+        // Move on to next question in array
+        game.currentQuestion++
+        // Load next question
+        game.loadQuestion();
+    },
     timeUp: function() {
-
+        // Stop the timer
+        clearInterval(timer);
+        // Add to unanswered counter
+        game.unanswered++;
+        // HTML says we ran out of time
+        $('#subwrapper').html('<h2>Times up, son!!</h2>');
+        // Tell them the correct answer in HTML
+        $('#subwrapper').append('<h3>The Correct Answer Was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        // If last question, take to results screen.  Otherwise, load nextQuestion
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     results: function() {
-
+        // Clearing timer
+        clearInterval(timer);
+        // Updating HTML to say finished and show results
+        $('#subwrapper').html('<h2>Fin!!</h2>');
+        $('#subwrapper').append('<h3>Correct: ' + game.correct + '</h3>');
+        $('#subwrapper').append('<h3>Incorrect: ' + game.incorrect + '</h3>');
+        $('#subwrapper').append('<h3>Unanswered: ' + game.unanswered + '</h3>');
+        // Appending reset button
+        $('#subwrapper').append('<button id="reset">RESET</button>');
     },
     clicked: function(e) {
         // Clear interval once answer is selected
@@ -91,12 +128,45 @@ var game = {
         }
     },
     answeredCorrectly: function() {
-        console.log("correct!")
+        console.log("correct!");
+        // Reset timer
+        clearInterval(timer);
+        // Update the correct score
+        game.correct++;
+        // Display Correct!! on html
+        $('#subwrapper').html('<h2>Correct!!</h2>');
+        // If last question, take to results screen.  Otherwise, load nextQuestion
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     answeredIncorrectly: function() {
-        console.log("wrong!")
+        console.log("wrong!");
+        // Reset timer
+        clearInterval(timer);
+        // Update the correct score
+        game.incorrect++;
+        // Display Incorrect!! on html
+        $('#subwrapper').html('<h2>Incorrect!!</h2>');
+        // Display correct answer
+        $('#subwrapper').append('<h3>The Correct Answer Was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+        // If last question, take to results screen.  Otherwise, load nextQuestion
+        if (game.currentQuestion == questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        } else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
     },
     reset: function() {
-
+        // Reset game to start
+        game.currentQuestion = 0;
+        game.counter = 0;
+        game.correct = 0;
+        game.incorrect = 0;
+        game.unanswered = 0;
+        // Load questions
+        game.loadQuestion();
     }
 };
